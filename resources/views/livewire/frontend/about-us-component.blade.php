3075fc1cc854e646e0ecd70aa138da918cee5999
Jaymoh-G <gmorembatia@gmail.com>
2025-02-12 08:18:59 +0300

    update

--- resources/views/livewire/frontend/about-us-component.blade.php
+++ resources/views/livewire/frontend/about-us-component.blade.php
@@ -234,7 +234,7 @@
                     <!-- About Content -->
                     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                                           <img src="{{ asset($testimonial->image) }}" alt="" srcset="" style="box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
-    width: 90px;
+    width: 140px;
     height: 90px;
 ">
                         <div class="about_text">
